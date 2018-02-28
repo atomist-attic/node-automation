b527e57956cad635ad45ec70d542d4c09dfcf7cd
Jessica Kerr <jessitron@gmail.com>
2018-02-28 16:38:20 -0600

    in sdm generator, handle multiline teamIds
    because it's currently that way in github-sdm

--- src/commands/editor/node/updateAtomistTeam.ts
+++ src/commands/editor/node/updateAtomistTeam.ts
@@ -5,8 +5,8 @@ import { Project } from "@atomist/automation-client/project/Project";
 export async function updateAtomistTeam(project: Project, ctx: HandlerContext) {
     const atomistConfigFile = await project.findFile("src/atomist.config.ts");
     const content = await atomistConfigFile.getContent();
-    const newContent = content.replace(/teamIds:.*$/m,
-        `teamIds: ["${ctx.teamId}"],`);
+    const newContent = content.replace(/teamIds:\s*\[[^\]]*]/,
+        `teamIds: ["${ctx.teamId}"]`);
     await atomistConfigFile.setContent(newContent);
     return project;
 }
---
+++ test/commands/editor/node/updateAtomistTeamTest.ts
@@ -0,0 +1,79 @@
+import "mocha";
+import * as assert from "power-assert";
+import { InMemoryProject } from "@atomist/automation-client/project/mem/InMemoryProject";
+import { updateAtomistTeam } from "../../../../src/commands/editor/node/updateAtomistTeam";
+import { HandlerContext } from "@atomist/automation-client";
+
+describe("update Atomist Team", () => {
+
+    const fakeContext = {
+        teamId: "TEAMYAY",
+    } as any as HandlerContext;
+
+    it("changes the team", (done) => {
+        const p = InMemoryProject.of({
+            path: "src/atomist.config.ts",
+            content:
+                `
+export const configuration: Configuration = {
+    name: pj.name,
+    version: pj.version,
+    teamIds: ["T5964N9B7"], // <-- run @atomist pwd in your slack team to obtain the team id
+    commands: assembled.commandHandlers.concat([
+        HelloWorld,
+        () => affirmationEditor,
+        () => applyHttpServicePhases,
+        () => breakBuildEditor,
+        () => unbreakBuildEditor,
+        () => javaAffirmationEditor,
+    ]),
+};
+`
+        });
+        updateAtomistTeam(p, fakeContext)
+            .then(
+                edited => edited.findFile("src/atomist.config.ts"))
+            .then(f => f.getContent())
+            .then(content => {
+                assert(content.includes(`teamIds: ["TEAMYAY"], // <-- run @atomist pwd in your slack team to obtain the team id`),
+                    "content: " + content);
+            })
+            .then(() => done(), done);
+    });
+
+    it("changes the team when it's multiline", (done) => {
+        const p = InMemoryProject.of({
+            path: "src/atomist.config.ts",
+            content:
+                `
+export const configuration: Configuration = {
+    name: pj.name,
+    version: pj.version,
+    teamIds: [
+        //  "T1JVCMVH7",
+        "T5964N9B7",    // spring-team
+        //  "T29E48P34",    // Atomist community
+    ],
+    commands: assembled.commandHandlers.concat([
+        HelloWorld,
+        () => affirmationEditor,
+        () => applyHttpServicePhases,
+        () => breakBuildEditor,
+        () => unbreakBuildEditor,
+        () => javaAffirmationEditor,
+    ]),
+};
+`
+        });
+        updateAtomistTeam(p, fakeContext)
+            .then(
+                edited => edited.findFile("src/atomist.config.ts"))
+            .then(f => f.getContent())
+            .then(content => {
+                assert(content.includes(`teamIds: ["TEAMYAY"],\n    commands:`),
+                    "content: " + content);
+            })
+            .then(() => done(), done);
+    });
+});
+
