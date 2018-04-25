416b7e2f1f3ca27e517e1a1f92d7f7f764e9d0ba
David Dooling <david@atomist.com>
2018-04-25 10:21:57 -0500

    Migrate event handler generator
    Move event handler generator from atomist-blogs/spring5-kotlin to this
    repository.  This handler is mentioned in
    https://the-composition.com/extending-your-slack-bot-part-2-events-6f2822c5eb6a
    so it needs to be maintained.

---
+++ src/commands/generator/EventHandlerSampleGenerator.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+    HandleCommand,
+    Parameters,
+} from "@atomist/automation-client";
+import { generatorHandler } from "@atomist/automation-client/operations/generate/generatorToCommand";
+import { ProjectPersister } from "@atomist/automation-client/operations/generate/generatorUtils";
+import { GitHubProjectPersister } from "@atomist/automation-client/operations/generate/gitHubProjectPersister";
+
+import { NodeGeneratorParameters } from "../editor/node/nodeGenerator";
+import { nodeTransform } from "../editor/node/nodeTransform";
+
+/**
+ * Creates an Atomist event handler automation GitHub repository.
+ * Describe in
+ * https://the-composition.com/extending-your-slack-bot-part-2-events-6f2822c5eb6a
+ */
+@Parameters()
+export class EventHandlerSampleGeneratorParameters extends NodeGeneratorParameters {
+
+    constructor() {
+        super();
+        this.source.owner = "atomist-blogs";
+        this.source.repo = "event-handler";
+        this.target.description = "an Atomist automation with a sample event handler";
+    }
+}
+
+export function automationGenerator(
+    projectPersister: ProjectPersister = GitHubProjectPersister,
+): HandleCommand<EventHandlerSampleGeneratorParameters> {
+
+    return generatorHandler(
+        nodeTransform,
+        EventHandlerSampleGeneratorParameters,
+        "eventHandlerSampleGenerator",
+        {
+            intent: ["generate event-handler-sample"],
+            tags: ["node", "npm", "typescript"],
+            projectPersister,
+        });
+}
